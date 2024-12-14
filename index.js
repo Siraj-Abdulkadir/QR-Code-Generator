9dc1a0f5ce36ccd924f5cc9a0a7a66d9805b9163
Siraj Abdulkadir <imsirajabdulkadir@gmail.com>
2024-12-14 11:48:28 +0300

    fixed the node

--- index.js
+++ index.js
@@ -1,5 +1,33 @@
-const generateButton = document.getElementById("generateButton")
+const generateButton = document.getElementById("generateButton");
+
+
+// import inquirer from 'inquirer';
+const inputBox= document.getElementById("inputBox").value;
+
+inputBox.addEventListener('keyup', onclickGenerator)
+
+
 
 function onclickGenerator(){
-    alert("You clicked me")
+    const inputBox= document.getElementById("inputBox").value;
+    console.log(inputBox)
+    alert(inputBox)
 }
+
+
+// inquirer
+//   inputBox.prompt([
+//     {message:"Enter the link for your Qr-Code",
+//     name:"URL"}
+//   ])
+//   .then((answers) => {
+//     alert(answers)
+//     const url = answers.URL;
+//   })
+//   .catch((error) => {
+//     if (error.isTtyError) {
+//       // Prompt couldn't be rendered in the current environment
+//     } else {
+//       // Something else went wrong
+//     }
+//   });
